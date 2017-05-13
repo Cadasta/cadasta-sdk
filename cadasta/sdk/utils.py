263a092e96546cb69711c931790160e930d1280b
Anthony Lukach <alukach@cadasta.org>
2017-05-12 22:26:49 -0600

    Add simularity function for fuzzy matching strings

--- cadasta/sdk/utils.py
+++ cadasta/sdk/utils.py
@@ -1,3 +1,6 @@
+from difflib import SequenceMatcher
+
+
 def slugify(string, max_length=50):
     """
     Naive slugify. WARNING: Results may not exactly match Cadasta's slugs.
@@ -6,3 +9,11 @@ def slugify(string, max_length=50):
     if max_length:
         string = string[:max_length]
     return string
+
+
+def similarity(str1, str2):
+    """
+    Return the percentage of simularity between two strings. Useful when
+    working with datasets that contain many typos.
+    """
+    return SequenceMatcher(None, str1, str2).ratio()
