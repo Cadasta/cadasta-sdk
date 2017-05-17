032d85387755763a5420e5779fc1efba9023d11a
Anthony Lukach <alukach@cadasta.org>
2017-05-17 11:56:34 -0600

    Update script

--- examples/data_import.py
+++ examples/data_import.py
@@ -72,6 +72,7 @@ about when to schedule followup tasks.
 
 import logging
 import os
+import zipfile
 
 from cadasta.sdk import connection, endpoints
 from cadasta.sdk.helpers import fs, geo, http, string, threading
@@ -100,30 +101,61 @@ cnxn = connection.CadastaSession(CADASTA_URL, username=USERNAME)
 # then whatever values are needed to get the job done. By sending work into the
 # queue, it allows our thread-workers to process them concurrently rather than
 # syncronously.
-def upload_location(q, org_slug, proj_slug, party_id, shp_path):
+def upload_location(q, org_slug, proj_slug, party_id, shp_path, layer):
     endpoint_url = endpoints.locations(org_slug, proj_slug)
-    relationships = []
-    for layer in geo.prepare_geodata(shp_path):
-        # TODO: Something is wrong here. Maybe we don't want every layer to be
-        # a SpatialUnit.
-        spatial_unit = cnxn.post(endpoint_url, json=layer).json()
-        su_id = spatial_unit['properties']['id']
+    spatial_unit = cnxn.post(endpoint_url, json=layer).json()
+    su_id = spatial_unit['properties']['id']
 
-        # Add relationship between location and party
-        tenure_endpoint = endpoints.tenure_relationships(org_slug, proj_slug)
-        relationships.append(cnxn.post(tenure_endpoint, json={
-            'party': party_id,
-            'spatial_unit': su_id,
-            'tenure_type': 'LH',  # TODO: Verify that this is correct (https://cadasta.github.io/api-docs/#relationship-json-object)
-            'attributes': {},
-        }).json())
+    # Add relationship between location and party
+    tenure_endpoint = endpoints.tenure_relationships(org_slug, proj_slug)
+    relationship = cnxn.post(tenure_endpoint, json={
+        'party': party_id,
+        'spatial_unit': su_id,
+        'tenure_type': 'LH',  # TODO: Verify that this is correct (https://cadasta.github.io/api-docs/#relationship-json-object)
+        'attributes': {},
+    }).json()
+
+    # q.put(zip_and_upload_resource, )
 
     # TODO:
-    #   - Zip up SHP files, upload as Relationship Resource
+    #   - Associate Location Resource with Party Resource Zip File (maybe this
+    #     isn't necessary, it feels difficult.)
     #   - Process GDB/ data as Location Resources
     #   - Process MXD/ data as Relationship Resources
     #   - Process Map/ data as Relationship Resources
     #   - Process Text/ data as Relationship Resources
+
+
+
+def parse_shapefile(q, org_slug, proj_slug, party_id, shp_path):
+    """
+    Given a shapefile, format data and either create Locations from layers
+    or simply upload shapefile as Party Resource.
+    """
+    uploaded = False
+    for layer in geo.prepare_geodata(shp_path):
+        if layer['geometry']['type'] != 'Polygon':
+            continue
+        q.put(upload_location, org_slug, proj_slug, party_id, shp_path, layer)
+        uploaded = True
+
+    # Zip up and upload shapefiles as Party Resource
+    with fs.TemporaryDirectory() as tmpdir:
+        shp_name = shp_path.split('/')[-1].split('.')[0]
+        zip_path = os.path.join(tmpdir, '{}.shp.zip'.format(shp_name))
+        with zipfile.ZipFile(zip_path, 'w') as tmp_zip:
+            logger.debug("Created zip %r", zip_path)
+            for f in fs.ls_files(os.path.dirname(shp_path)):
+                if not f.split('/')[-1].startswith(shp_name):
+                    continue
+                logger.debug("Adding %r to %r", f, zip_path)
+                tmp_zip.write(f)
+        # Being that we want to do this before the temporary directory is
+        # automatically deleted, we need to run this syncronously. There's
+        # not a lot of downside to this as doing this asyncronously offers
+        # no benefit since this is a single task that couldn't be run in
+        # parallel.
+        upload_party_resource(q, org_slug, proj_slug, party_id, zip_path)
 
 
 def upload_party_resource(q, org_slug, proj_slug, party_id, resource_path):
@@ -141,7 +173,8 @@ def upload_party_resource(q, org_slug, proj_slug, party_id, resource_path):
         'original_file': original_file,
     }
     mime_type = http.get_mime_type(resource_path)
-    if mime_type:
+    if mime_type and 'zip' not in mime_type:
+        # FIXME: Zip mimetypes not currently supported.
         resource_data.update(mime_type=mime_type)
     resource = cnxn.post(endpoint_url, json=resource_data).json()
 
@@ -187,15 +220,14 @@ def create_party(q, org_slug, proj_slug, party_dir):
         # We're handle the Party Resources and Locations here. We can't yet
         # handle the Location Resources or Relationship Resources as we haven't
         # yet uploaded the Locations.
-        if name in ('photo',):
+        if name in ('photo', 'gdb'):
             for path in fs.ls_files(d):
                 # Schedule 'upload_party_resource' for each photo
-                q.put(upload_party_resource, org_slug, proj_slug,
-                      party_id, path)
+                q.put(upload_party_resource, org_slug, proj_slug, party_id, path)
         if name in ('shp',):
             files = [f for f in fs.ls_files(d) if f.endswith('.shp')]
             for path in files:
-                q.put(upload_location, org_slug, proj_slug, party_id, path)
+                q.put(parse_shapefile, org_slug, proj_slug, party_id, path)
 
 
 def create_project(q, org_slug, proj_dir, **kwarg):
